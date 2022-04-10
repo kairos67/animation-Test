a408e205b37fcc462d7f888e27341e74077a2644
kairos67 <kairosys@gmail.com>
2022-04-10 16:24:01 +0900

    first commit

---
+++ main.java
@@ -0,0 +1,6 @@
+class Main{
+public static void main(String[] args){
+    int a = 12;
+    System.out.println(a);
+}
+}
---
+++ script.js
@@ -0,0 +1,13 @@
+// let names = ["1", "2", "3", "4"]
+
+// // names.forEach((item,index)=>{console.log(item,index)})
+
+// let data = names.map((item) => {
+//     return item+"하하하"
+// })
+// console.log(data)
+
+
+//console.log(a)
+let a=1
+console.log(a)
